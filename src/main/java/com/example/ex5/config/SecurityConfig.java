a58aa4f1e809027af317b0fc592b703bd6a0542e
Tamar Ayache <tamarayash12@gmail.com>
2024-06-18 22:21:44 +0300

    add securityConig

---
+++ src/main/java/com/example/ex5/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.ex5.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/login", "/json", "/error","/signupnews" ,"/news/**", "/index/**","/").permitAll()
+                                .requestMatchers("/edit/**", "/delete/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/", true)
+                                .permitAll()
+                )
+                .logout(logout ->
+                        logout
+                                .logoutSuccessUrl("/login?logout")
+                                .permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+}
--- src/main/java/com/example/ex5/controllers/NewsController.java
+++ src/main/java/com/example/ex5/controllers/NewsController.java
@@ -3,24 +3,23 @@ package com.example.ex5.controllers;
 import com.example.ex5.repo.News;
 import com.example.ex5.repo.NewsRepository;
 import com.example.ex5.services.NewsService;
-import jakarta.validation.*;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 @Controller
 public class NewsController {
 
-    /* example: injecting a default value from the application.properties  file */
-    @Value( "${demo.coursename}" )
+    @Value("${demo.coursename}")
     private String someProperty;
 
-    /* inject via its type the News repo bean - a singleton */
     @Autowired
     private NewsRepository repository;
 
@@ -35,21 +34,11 @@ public class NewsController {
         return "index";
     }
 
-
     @GetMapping("/signupnews")
     public String showSignUpForm(News news, Model model) {
-        //model.addAttribute("news", new news("noname","noemail"));
         return "add-news";
     }
 
-    /**
-     * because we declared a BindingResult parameter, Spring will pass it to the controller
-     * and we MUST use it to check for validation errors and catch them
-     * @param news
-     * @param result
-     * @param model
-     * @return
-     */
     @PostMapping("/addnews")
     public String addNews(@Valid News news, BindingResult result, Model model) {
         if (result.hasErrors()) {
@@ -58,53 +47,24 @@ public class NewsController {
 
         repository.save(news);
 
-        // הפניה מחדש למניעת שליחת טופס כפולה
         return "redirect:/news";
     }
 
-    /** we implemented a POST request for adding a news, as a result
-     * if the news accesses the /addnews URL with a GET request, we must redirect him to the main page
-     * @param news
-     * @param model
-     * @return
-     */
     @GetMapping("/addnews")
     public String showAddNewsForm(News news, Model model) {
-        // redirect to main page
         return "redirect:/";
     }
 
-
-    /**
-     * USING GET HTTP METHODS FOR UPDATING AND DELETING IS NOT A GOOD PRACTICE
-     * below you will find the same methods but using POST methods
-     * @param id
-     * @param model
-     * @return
-     */
     @GetMapping("/edit/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public String showUpdateForm(@PathVariable("id") long id, Model model) {
-
-        News news = repository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("Invalid news Id:" + id));
-
-        // the name "news"  is bound to the VIEW
+        News news = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid news Id:" + id));
         model.addAttribute("news", news);
         return "update-news";
     }
 
-    /* same as above but PostMapping
-    @PostMapping("/edit")
-    public String news(@RequestParam("id") long id, Model model) {
-
-        News news = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid news Id:" + id));
-
-        // the name "news"  is bound to the VIEW
-        model.addAttribute("news", news);
-        return "update-news";
-    }    */
-
     @PostMapping("/update/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public String updateNews(@PathVariable("id") long id, @Valid News news, BindingResult result, Model model) {
         if (result.hasErrors()) {
             news.setId(id);
@@ -122,25 +82,14 @@ public class NewsController {
     }
 
     @PostMapping("/delete")
-    public String News(@RequestParam("id") long id, Model model) {
-
-        // we throw an exception if the news is not found
-        // since we don't catch the exception here, it will fall back on an error page (see below)
-        News news = repository
-                .findById(id)
-                .orElseThrow(
-                        () -> new IllegalArgumentException("Invalid news Id:" + id)
-                );
+    @PreAuthorize("hasRole('ADMIN')")
+    public String deleteNews(@RequestParam("id") long id, Model model) {
+        News news = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid news Id:" + id));
         repository.delete(news);
         model.addAttribute("newsp", repository.findAll());
         return "index";
     }
 
-    /**
-     * USING GET HTTP METHODS FOR UPDATING AND DELETING IS NOT A GOOD PRACTICE
-     * @param id
-     * @return
-     */
     @GetMapping("/delete/{id}")
     public String deleteNewsGet(@PathVariable long id) {
         return "redirect:/";
@@ -151,24 +100,13 @@ public class NewsController {
         return "redirect:/";
     }
 
-    /**
-     * returns the json.html page
-     * @param model
-     * @return
-     */
-    @GetMapping(value="/json")
-    public String json (Model model) {
+    @GetMapping(value = "/json")
+    public String json(Model model) {
         return "json";
     }
-    /**
-     * a sample controller return the content of the DB in JSON format
-     * @return
-     */
 
-    /** handling IllegalArgumentException */
     @GetMapping("/error")
     public String error() {
         return "error";
     }
-
 }
